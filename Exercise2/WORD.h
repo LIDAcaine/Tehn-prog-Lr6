87a1567685b427a6c3bdd6e8ab6358aaf979d75c
LIDAcaine <99477134+LIDAcaine@users.noreply.github.com>
2022-12-01 21:18:28 +0300

    Add files via upload

---
+++ Exercise2/WORD.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <iostream>
+#include <sstream>
+#include <fstream>
+
+using namespace std;
+
+class WORD
+{
+private:
+	string word;
+public:
+	void setword(string wrd);
+	string getword() const;
+	void change();
+	friend ostream& operator <<(ostream& output, WORD& pointer);
+};
---
+++ Exercise2/main.cpp
@@ -0,0 +1,55 @@
+﻿#include "WORD.h"
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#define ERROR_101 101
+#define ERROR_102 102
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    stringstream ss;
+    string str;
+    string temp;
+    WORD buff;
+    ifstream fin;
+    fin.open("test.txt");
+    try
+    {
+        if (!fin.is_open())
+            throw ERROR_101;
+    }
+    catch (int error)
+    {
+        cout << "Файла с таким именем не существует в директории!" << endl;
+        return ERROR_101;
+    }
+    cout << "Задание №2: Работа с текстом" << endl;
+    while (!fin.eof())
+    {
+        getline(fin, str);
+        ss << str;
+        while (ss >> temp)
+        {
+            buff.setword(temp);
+            buff.change();
+            cout << buff << " ";
+        }
+        cout << endl;
+        ss.clear();
+    }
+    try
+    {
+        if (!fin.is_open())
+            throw ERROR_102;
+    }
+    catch (int error)
+    {
+        cout << "Файл не может быть закрыт!" << endl;
+        return ERROR_102;
+    }
+    fin.close();
+    return 0;
+}
